0d9e01bc8760f1b9623b736d2794e6e2caf98203
khun <khunstr@protonmail.com>
2023-04-25 12:25:29 +0000

    commit

--- program/main.py
+++ program/main.py
@@ -1,37 +1,62 @@
 import time, json, random
 
 def login():
-    account = input("do you have an account? (y/n)")
-    if account == "Y" or "y":
+    account = input("do you have an account with ocr-tunes? (y/n) ")
+    if account == "y":
         print("redirecting")
         accAccess()
-    elif account == "N" or "n":
+    elif account == "n":
         print("new to ocr-tunes? create an account")
         accCreate()
+    else:
+        print("invalid option")
+        print("please try again")
+        login()
 
+""" --WIP
 def spotifyConnect():
-    spotifyAuth = input("connect to spotify? (y/n)")
-    if spotifyAuth == "Y" or "y":
+    spotifyAuth = input("connect to spotify? (y/n) ")
+    if spotifyAuth == "y":
         return True
-    elif spotifyAuth == "N" or "n":
+    elif spotifyAuth == "n":
         return False
     else:
         print("error cannot process request")
         print("please try again")
         spotifyConnect()
+"""
 
 def openusersfile():
-    with open('users.json') as f:
+    with open('users.json',encoding='UTF-8') as f:
         users = json.loads(f.read())
         return users
 
 def main():
     print("Welcome to ocr-tunes")
-    spotifyConnect()
+    #spotifyConnect() --WIP
     login()
 
 def accAccess():
-    print()
+    username = str(input("input your username "))
+    password = str(input("input your password "))   
+
+    if auth(username, password):
+        print("account found")
+        menu()
+    else:
+        print("no account matches\n try again")
+        accAccess()
 
 def accCreate():
-    print()
+    """"""
+
+def auth(username, password):
+    fileopen = openusersfile()
+    for i in fileopen:
+        print(i)
+
+def menu():
+    """"""
+
+if __name__ == "__main__":
+    main() 
